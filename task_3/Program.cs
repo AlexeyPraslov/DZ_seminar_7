0ee777f7a9f9fad09ba38e362e33fa616a0bb3b1
Alexey <ipad2322012@gmail.com>
2023-12-08 14:17:16 +0300

    добавили 3 задачу

---
+++ task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " ");
+
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
+
